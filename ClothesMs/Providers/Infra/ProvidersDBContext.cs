d5b1ee79728d615840ed4eee03f724e6b2c5076d
Ismael Puricelli <ipuricelli@urudata.com>
2022-08-09 23:57:31 -0300

    Se agrega dominio de Providers.

---
+++ ClothesMs/Providers/Infra/ProvidersDBContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Providers.Domain;
+using System.Reflection;
+
+namespace Providers.Infra
+{
+    public class ProvidersDBContext : DbContext
+    {
+        public ProvidersDBContext(DbContextOptions<ProvidersDBContext> options) : base(options)
+        {
+
+        }
+
+        public ProvidersDBContext()
+        {
+
+        }
+
+        public DbSet<Provider> Providers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            //this is necessary, because EF Core, doesn`t have the method "Conventions"
+            //that we use to make the configuration of the tables
+            modelBuilder.Entity<Provider>(b =>
+            {
+                b.HasKey(p => p.Id);
+                b.Property(p => p.Name).IsRequired();
+            });
+        }
+    }
+}
+
--- ClothesMs/Providers/Startup.cs
+++ ClothesMs/Providers/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Providers.Infra;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,9 @@ namespace Providers
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<ProvidersDBContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("Default")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
